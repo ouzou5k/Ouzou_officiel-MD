8806aebdf05077295fe2889274d2e1611b639e29
ouzou5k <ouzou.officiel7744@gmail.com>
2025-08-26 00:38:11 +0000

    Update env.js

--- env.js
+++ env.js
@@ -1,1 +1,7 @@
+require('dotenv').config();
 
+module.exports = {
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "1234567890@s.whatsapp.net",
+  SESSION_ID: process.env.SESSION_ID || "session.json",
+  BOT_NAME: process.env.BOT_NAME || "OuzouBot"
+};
